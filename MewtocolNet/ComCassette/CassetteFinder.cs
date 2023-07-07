864389a4ad385b9cd95eba60080f4619f14e2d73
Felix Weiß <72068105+Sandoun@users.noreply.github.com>
2023-07-07 16:46:53 +0200

    Fixed LinqHelpers namespace

--- MewtocolNet/ComCassette/CassetteFinder.cs
+++ MewtocolNet/ComCassette/CassetteFinder.cs
@@ -1,5 +1,4 @@
-﻿using MewtocolNet.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
--- MewtocolNet/Helpers/LinqHelpers.cs
+++ MewtocolNet/Helpers/LinqHelpers.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MewtocolNet.Helpers {
+namespace MewtocolNet {
 
     internal static class LinqHelpers {
 
