4d6eee558504eee44ab25fa23eb39f10f46d6f9b
Felix Weiß <72068105+Sandoun@users.noreply.github.com>
2023-07-26 00:20:01 +0200

    Added missing F functions set

--- MewtocolNet/PublicEnums/PlcVarType.cs
+++ MewtocolNet/PublicEnums/PlcVarType.cs
@@ -3,16 +3,19 @@
     public enum PlcVarType {
 
         BOOL,
+        WORD,
         INT,
         UINT,
+        DWORD,
         DINT,
         UDINT,
         REAL,
         TIME,
+        DATE_AND_TIME,
+        DATE,
+        TIME_OF_DAY,
         STRING,
-        WORD,
-        DWORD
-
+   
     }
 
 }
